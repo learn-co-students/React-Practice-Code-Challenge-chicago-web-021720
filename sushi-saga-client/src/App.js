4bf6d5662a71b1555cf0db8b1a7d6bc1154ee25e
cooljasonmelton <cooljasonmelton>
2020-04-28 10:36:28 -0500

    Done.

--- sushi-saga-client/src/App.js
+++ sushi-saga-client/src/App.js
@@ -6,12 +6,64 @@ import Table from './containers/Table';
 const API = "http://localhost:3000/sushis"
 
 class App extends Component {
+  constructor(){
+    super()
+    this.state ={
+      sushis: [],
+      money: 100,
+      plates: []
+    }
+  }
+
+  componentDidMount(){
+    fetch(API)
+    .then(r=>r.json())
+    .then(fetSushis => {
+      const sushis = fetSushis.map(sushi => {
+        const newObj = Object.assign({}, sushi)
+        newObj.isEaten = false;
+        return newObj
+      })
+      this.setState({ sushis: [...sushis] })
+    })
+  }
+
+  deleteSushi = () => {
+    const updatedSushis = this.state.sushis.filter(sushi =>{
+      return !sushi.isEaten 
+    })
+    this.setState({  
+      sushis: [...updatedSushis]
+    })
+  }
+
+  eatSushi = (id) => {
+    let sushiPrice = 0
+    const updatedSushis = this.state.sushis.map(sushi =>{
+      if (sushi.id === parseInt(id)){
+        if (sushi.price < this.state.money && !sushi.isEaten) {
+          sushiPrice = sushi.price
+          sushi.isEaten = true
+          this.setState(prevState => ({  
+            sushis: [...updatedSushis],
+            money: prevState.money - sushiPrice,
+            plates: [...prevState.plates, 1] 
+          }))
+        } 
+      }
+      return sushi
+    })
+
+  }
+
+  
+
 
   render() {
     return (
       <div className="app">
-        <SushiContainer />
-        <Table />
+        <SushiContainer sushis={this.state.sushis} eatSushi={this.eatSushi}deleteSushi={this.deleteSushi}/>
+        <Table plates={this.state.plates} money={this.state.money} />
       </div>
     );
   }
